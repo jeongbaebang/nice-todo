635706269044595454d42928babbb6d115f6c4fd
jeongbaebang <jeongbaebang_dev@naver.com>
2024-08-20 10:19:58 +0900

    Add FormHandler 리팩토링 & 클래스 메서드 분리

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,69 @@
 import { FormHandler } from './form';
+import { $ } from './utils';
 
-const form = new FormHandler();
+class TodoApp {
+  constructor() {
+    this.todoItems = [];
+    this.container = $('.item-list__container');
+  }
+
+  addItem(value) {
+    if (typeof value !== 'string' || value === '') {
+      throw new Error('Make sure you pass the correct string.');
+    }
+
+    const item = {
+      id: Date.now(),
+      text: value,
+      completed: false,
+    };
+
+    this.todoItems.push(item);
+    this.#renderItem(item);
+  }
+
+  #renderItem(item) {
+    const itemHTML = this.#generateItem(item);
+    const template = document.createElement('template');
+    template.innerHTML = itemHTML.trim();
+    const newItem = template.content.firstChild;
+
+    this.container.insertAdjacentElement('afterbegin', newItem);
+  }
+
+  /**
+   * @param {Object} item
+   * @param {number} item.id
+   * @param {number} item.text
+   * @param {number} item.completed
+   * @returns
+   */
+  #generateItem(item) {
+    if (!item?.id || !item?.text) {
+      throw new Error('You must provide valid attributes.');
+    }
+
+    const { id, text, completed } = item;
+
+    return `
+      <div class="task__component" data-key="${id}">
+        <div class="checkbox__component">
+          <label>
+            <input type="checkbox" ${completed ? 'checked' : ''}/>
+          </label>
+        </div>
+        <div class="input__component">
+          <input type="text" disabled placeholder="텍스트를 입력하세요" value="${text}"/>
+        </div>
+        <button class="button__component" data-type="deletion">
+          <span class="material-symbols-outlined"> delete </span>
+        </button>
+      </div>
+    `;
+  }
+}
+
+const todo = new TodoApp();
+const form = new FormHandler({ onSubmit: todo.addItem.bind(todo) });
 
 document.addEventListener('submit', form.handler.bind(form));
--- src/js/form.js
+++ src/js/form.js
@@ -2,16 +2,19 @@ import { $ } from './utils';
 
 export class FormHandler {
   /**
-   * @type {?string}
+   * @typedef {function(string): void} OnSubmit
+   * @param {Object} params
+   * @param {OnSubmit} params.onSubmit - 아이템이 제출될 때 호출되는 콜백 함수
    */
-  #key = 0;
-
-  constructor() {
+  constructor({ onSubmit }) {
     this.form = $('#todo-form');
     this.input = $('.input__component[data-type="creation"] > input');
-    this.parentElement = $('.item-list__container');
+    this.callback = onSubmit;
   }
 
+  /**
+   * @param {string} value
+   */
   #checkValue(value) {
     if (value === '') {
       window.alert('빈 문자열은 아이템 생성이 불가능합니다.');
@@ -39,38 +42,8 @@ export class FormHandler {
     const value = this.#checkValue(this.input.value);
 
     if (value) {
-      this.renderItem(value);
+      this.callback(value);
+      this.#clearValue();
     }
-  }
-
-  generateItem(value) {
-    return `
-      <div class="task__component" data-key="${this.#key}">
-        <div class="checkbox__component">
-          <label>
-            <input type="checkbox" />
-          </label>
-        </div>
-        <div class="input__component">
-          <input type="text" disabled placeholder="텍스트를 입력하세요" value="${value}"/>
-        </div>
-        <button class="button__component" data-type="deletion">
-          <span class="material-symbols-outlined"> delete </span>
-        </button>
-      </div>
-    `;
-  }
-
-  renderItem(value) {
-    const itemHTML = this.generateItem(value);
-    // 템플릿 요소를 사용하여 HTML 문자열을 DOM 요소로 변환
-    const template = document.createElement('template');
-    template.innerHTML = itemHTML.trim(); // 공백 제거
-    const newItem = template.content.firstChild;
-
-    this.parentElement.insertAdjacentElement('afterbegin', newItem);
-    this.#key++;
-
-    this.#clearValue();
   }
 }
