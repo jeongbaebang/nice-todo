e11e5e183dae32b7a678cba7a5e086e50ddcdce7
jeongbaebang <jeongbaebang_dev@naver.com>
2024-08-20 12:34:10 +0900

    Add 다크/라이트 테마 상호작용 추가

--- src/js/app.js
+++ src/js/app.js
@@ -5,12 +5,33 @@ class TodoApp {
   /**
    * @type {{ id: number, text: string, completed: boolean }[]}
    */
-  todoItems;
+  todoItems = [];
 
   constructor() {
     this.container = $('.item-list__container');
     this.#loadItemsFromStorage();
     this.#initRenderItem();
+    this.#addThemeEvent();
+  }
+
+  #addThemeEvent() {
+    const button = $('.button__component[data-type="theme"]');
+
+    if (!button) {
+      throw new Error('Invalid theme button');
+    }
+
+    const currentTheme = localStorage.getItem('theme') || 'dark';
+
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    button.addEventListener('click', () => {
+      let theme = document.documentElement.getAttribute('data-theme');
+      theme = theme === 'light' ? 'dark' : 'light';
+
+      document.documentElement.setAttribute('data-theme', theme);
+      localStorage.setItem('theme', theme);
+    });
   }
 
   #initRenderItem() {
